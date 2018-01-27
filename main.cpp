6510e04261f70fee049066e09554d8f85f6d992b
mohamed-elfiky <elfikymohamedelfiky@gmail.com>
2018-01-27 18:16:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,30 @@
-/*mohamed elfiky*/
-/*recursive decent algorithm*/
 #include<iostream>
 #include<string>
 #include<sstream>
 #include<cmath>
+#include<map>
+#include<algorithm>
 using namespace std ;
-const char number = '8';
+const char num = '1';
+const char opsy='5' ;
+const char iden='9' ;
+map<string, double >lookup;
 //////////////////////////////////////////////
+void error (string s)
+
+{
+  throw runtime_error (s);
+}
 struct Token
 {
     char kind;
 	double value;
-	Token(char ch) :kind(ch), value(0) { }
+	char alpha ;
+    string ident;
+    Token(char ch) :kind(ch), value(0) { }
 	Token(char ch, double val) :kind(ch), value(val) { }
+    Token(char alph , char ki):alpha(alph) ,kind(ki){ }
+    Token(char ch , string  id) :kind(ch) , ident(id){ }   
 };
 ///////////////////////////////////////////////////////////
 stringstream math_exp;
@@ -24,7 +36,7 @@ double eval(string) ;
 ////////////////////////////////////////////////////////////
 int main()
 {
-cout<<eval("1+2/2;");
+cout<<eval("5.5+12*log(0.4)+2.2^4;");
 return 0;
 }
 ////////////////////////////////////////////
@@ -38,8 +50,6 @@ double factor()
 {
 Token t = get_token();
 double left;
-
-
 while(true)
 {
 switch (t.kind)
@@ -47,7 +57,7 @@ switch (t.kind)
 case '(':
 {
         left = expression();
-        t = get_token();if(t.kind!=')')return 0;
+        t = get_token();if(t.kind!=')'){ error(" missing )");}// should return error here
         t = get_token();
         break;
 }
@@ -55,17 +65,83 @@ case '^':
         left=pow(left,factor());
         t = get_token();
         break;
-case '8':
+case '1':
         left= t.value;
         t = get_token();
         break;
-default:
+
+case '5':
+        {
+
+                            string s;
+                            char ch=t.alpha;
+                            cout<<t.alpha<<endl;
+                            while(true)
+                            {
+                            s+=ch;
+                            math_exp>>ch;
+                            cout<<s<<endl;
+                            if(s== "sin" )
+                                        {
+                                          double d =expression();
+                                          d=sin(d);
+                                          math_exp>>ch;
+
+                                          return d;
+                                         }
+                            else if (s== "cos" )
+                                             {
+                                            double d =expression();
+                                            cout<<d<<"low"<<endl;
+                                            d=sin(d);
+                                            math_exp>>ch;
+
+                                            return d;
+                                            }
+                            else if (s=="tan" )
+                                        {
+                                          double d =expression();
+                                          cout<<d<<"low"<<endl;
+                                          d=tan(d);
+                                          math_exp>>ch;
+
+                                          return d;
+                                         }
+
+                            else if (s=="log" )
+                                        {
+                                          double d =expression();
+                                          cout<<d<<"low"<<endl;
+                                          d=log(d);
+                                          math_exp>>ch;
+
+                                          return d;
+                                         }
+
+                            else if (s=="sqrt" )
+                                         {
+                                          double d =expression();
+                                          cout<<d<<"low"<<endl;
+                                          d=sqrt(d);
+                                          math_exp>>ch;
+
+                                          return d;
+                                         }
+
+                        }
+                }
+
+
+ default:
         math_exp.putback(t.kind);
         return left;
-}
-}
-}
+
+    }
+  }
+ }
 ///////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
 double term()
 {
 double left = factor();
@@ -79,10 +155,13 @@ case '*':
         t = get_token();
         break;
 case '/':
-        left =left/ factor();
+        {
+        double d = factor() ;
+        if(d==0) error("division by zero");
+        left =left/ d;
         t = get_token();
         break;
-
+        }
 default:
         math_exp.putback(t.kind);
         return left;
@@ -99,7 +178,10 @@ while(true)
 		switch(t.kind)
 		{
 		case '+':
+                cout<<"here5"<<endl;
                 left += term();
+                                cout<<term<<endl;
+
                 t = get_token();
                 break;
         case '-':
@@ -108,6 +190,7 @@ while(true)
                 break;
          default:
                 math_exp.putback(t.kind);
+                //cout<<left<<endl;
                 return left;
 		}
 }
@@ -115,36 +198,67 @@ while(true)
 Token get_token()
 {
 
+    cout<<"here"<<endl;
+
 	char ch;
     math_exp>> ch;
-	switch (ch)
-	{
-	case '(':
-	case ')':
-	case '+':
-	case '-':
-	case '*':
-	case '/':
-	case '%':
-	case '^':
-	case '=':
-		return Token(ch);
-	case '.':
-	case '0':
-	case '1':
-	case '2':
-	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
+	if (ch== '(') 		return Token(ch);
+	else if (ch== ')')	return Token(ch);
+	else if (ch== '+')	return Token(ch);
+	else if (ch== '-')	return Token(ch);
+	else if (ch== '*')	return Token(ch);
+	else if (ch== '/')	return Token(ch);
+	else if (ch== '%')  return Token(ch);
+ 	else if (ch== '^')	return Token(ch);
+	else if (ch== '=')	return Token(ch);
+    else if(
+      (ch=='.')||
+	  (ch=='0')||
+	  (ch=='1')||
+	  (ch=='2')||
+	  (ch=='3')||
+	  (ch=='4')||
+	  (ch=='5')||
+	  (ch=='6')||
+	  (ch=='7')||
+	  (ch=='8')||
+	  (ch=='9'))
             {	math_exp.putback(ch);
                 double val;
                 math_exp >> val;
-                return Token(number,val);
+                return Token(num,val);
             }
 
-    }
+           else if(isalpha(ch))
+            {
+                            char ch1;
+                            math_exp>>ch1;      //lookahead
+                            if(isalpha(ch1)) 
+                            {
+                            math_exp.putback(ch1);
+                            math_exp.putback(ch);
+                            math_exp>>ch;
+                            return Token(ch,opsy);
+                            }
+
+
+                            else // idendtifier is called
+                            {
+                            math_exp.putback(ch);
+                            string k;
+                            k =ch;
+                            double d =lookup[k];
+                            return Token(k,d);
+                            }
+
+            }
+else
+    return 0;
 }
+
+
+
+
+
+
+
